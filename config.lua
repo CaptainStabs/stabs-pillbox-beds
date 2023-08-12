4bebc720cd4b843cc8d7e0f284a5220d090f25be
CaptainStabs <40151222+CaptainStabs@users.noreply.github.com>
2023-08-12 18:04:11 -0400

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,5 +1,5 @@
 Config = {}
 Config.MinimalDoctors = 2
-Config.AIHealWait = 60 -- How long it takes for the doctor to be "paged" before a local doctor can treat them.
+Config.AIHealWait = 20 -- How long it takes for the doctor to be "paged" before a local doctor can treat them.
 Config.AIHealTimer = 20 -- How long it will take to be healed after checking in, in seconds
 Config.Dispatch = 'qs-dispatch' -- Which dispatch framework you are using. Either `qs-dipatch` or `cd_dispatch`
